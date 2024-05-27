284d7e0a9839c58364b4dec777f17c7b3103f52e
AyunHarahel <harahelayun54@gmail.com>
2024-05-27 11:08:24 -0300

    agregando ejercicios

---
+++ Guia 6/Ejercicio 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numeros = new double[10];
+            Console.WriteLine("ingrese 10 numeros:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("numero " + (i + 1) + ": ");
+                numeros[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            double suma = 0;
+            foreach (double numero in numeros)
+            {
+                suma += numero;
+            }
+            double promedio = suma / 10;
+            Console.WriteLine("el promedio de los números ingresados es: " + promedio);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Guia 6/ejercicio 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese el valor máximo para calcular la suma:");
+            int maximo = Convert.ToInt32(Console.ReadLine());
+
+            int suma = (maximo * (maximo + 1)) / 2;
+
+            Console.WriteLine("la suma de los números naturales hasta " + maximo + " es: " + suma);
+            Console.ReadKey();
+        }
+    }
+}
