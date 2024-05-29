96d614979f4220821e54a2b3540f3823ae693aa8
AyunHarahel <harahelayun54@gmail.com>
2024-05-28 22:37:02 -0300

    se agrego ejercicio 5

---
+++ Guia 6/Ejercicio 5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, cont;
+            Console.WriteLine("Ingrese un numero");
+            num = Convert.ToInt32(Console.ReadLine());
+            cont = 0;
+            if (num < 4)
+            {
+                Console.WriteLine("el numero es primo");
+                Console.ReadKey();
+            }
+            else
+            {
+                for (int n = 1; n <= num; n++)
+                {
+                    if (num % n == 0)
+                    {
+                        cont++;
+
+                    }
+                    else { }
+                }
+                if (cont == 2)
+                    
+                {
+                    Console.WriteLine("Es primo");
+                }
+                else
+                {
+                    Console.WriteLine("no es primo");
+                }
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
